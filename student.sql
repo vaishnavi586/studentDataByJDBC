65a727117feaf43d1da11b1584040ac601dc4218
vaishnavi586 <59246665+vaishnavi586@users.noreply.github.com>
2020-11-12 14:13:56 +0530

    Add files via upload
    added student database

---
+++ student.sql
@@ -0,0 +1,10 @@
+create database student;
+use student;
+create table data (rollnumber int,studentname varchar(15));
+insert into data values('1','vaishu');
+insert into data values('2','harshi');
+insert into data values('3','anjana');
+insert into data values('4','emily');
+insert into data values('5','Shakspere');
+insert into data values('6','frost');
+select * from data;
